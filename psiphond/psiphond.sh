0d462dbf889c64c0cdec09da4cda0f25b73836c2
Farukbrowser <40906618+Farukbrowser@users.noreply.github.com>
2023-07-20 21:46:28 +0100

    Update psiphond.sh

--- psiphond/psiphond.sh
+++ psiphond/psiphond.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 ### by dankelthaher - scripts ###
-
 ## PSIPHOND SCRIPT DE AUTOINSTALACON 
-
 ## VARIABLES DE COLORES
 #COLORES 
 blanco='\033[1;38;5;231m'
@@ -17,19 +14,15 @@ guinda='\033[1;38;5;161m'
 rojo='\033[1;31m'
 cyanR='\033[1;38;5;87m'
 cierre='\033[0m'
-
 bar1="\e[1;30m◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚◚\e[0m"
 bar2="\033[38;5;226m---------------------------------------------------------\033[0m"
 bar3="\033[38;5;226m--------------------- = MENU = --------------------------\033[0m"
-
 #RUTAS DE ARCHIVOS
 cred1=".credenciales"
 dir_main="/root/psi"
 file_ip="/root/ip"
 file_init="/root/psi/psiphond"
-
 #ENLACES DE ARCHIVOS
-
 ## INSTALANDO BINARIO
 binario_script(){
 [[ ! -d $dir_main ]] && mkdir $dir_main
@@ -44,7 +37,6 @@ echo -e "$ipvps" > $file_ip
         ./psiphond --ipaddress 0.0.0.0 --web 3000 --protocol FRONTED-MEEK-HTTP-OSSH:3001 --protocol SSH: 3002 --protocol OSSH: 3003 --protocol FRONTED-MEEK-OSSH: 3004 generate
         apt install screen -y
 }
-
 ## INSTALLATION
 instalar_psi(){
     clear
@@ -61,7 +53,6 @@ instalar_psi(){
                 # START THE SERVICE HERE
 		screen
                 ./psiphond run
-
                 if [[ "$(ps x | grep 'psiphond' | grep -v 'grep'|wc -l)" != '0' ]]; then
 				   echo -e "${azulRB}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                    echo -e "${verde34} SERVICE STARTED SUCCESSFULLY IN PORT 3004"
@@ -85,11 +76,10 @@ instalar_psi(){
     exit 1
     fi
     ## FUNCIONES 
-    
-   
     echo ""
     echo -ne "\033[1;31mENTER \033[1;33mto return to \033[1;32mMENU!\033[0m" && read enter && clear
 }
     clear
     binario_script
     instalar_psi
+    
